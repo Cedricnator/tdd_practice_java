f268761af4d7a2fa0de4f118664a38dbe43de63e
cedricnator <c.kirmayr01@ufromail.cl>
2024-06-10 11:06:10 -0400

    first commit

---
+++ src/main/java/com/example/moiko_tdd_practice/MoikoTddPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.moiko_tdd_practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoikoTddPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoikoTddPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/moiko_tdd_practice/MoikoTddPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moiko_tdd_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoikoTddPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
